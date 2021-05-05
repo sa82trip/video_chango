189d53b37be1f65021339b76a135549f4ffeea28
Joonsung Gil <sa82trip@gmail.com>
2021-05-06 07:45:19 +0900

    modal for mobile menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,64 +1,119 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import firebase from "firebase";
 import { Link, Redirect, useHistory } from "react-router-dom";
 import { SORTING_METHOD } from "../pages/main-page";
 import mainLogo from "../icon/horizontal_on_white_by_logaster-removebg-preview.png";
+import justLogo from "../icon/just_logo.png";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faSearch,
+  faUser,
+  faArrowLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useForm } from "react-hook-form";
+import ReactModal from "react-modal";
+import { bugerMenuStyle, customStyles } from "../styles/modalStyle";
+
+interface ISearchForm {
+  searchTerm: string;
+}
 
 interface IHeaderProps {
   searchTerm: string;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   addVideoToWatchLaterList: () => void;
   handleSorting: (method: SORTING_METHOD) => void;
+  setIsModalOpen: Dispatch<SetStateAction<boolean>>;
 }
 export const Header: React.FC<IHeaderProps> = ({
   searchTerm,
   handleChange,
   addVideoToWatchLaterList,
   handleSorting,
+  setIsModalOpen,
 }) => {
-  const history = useHistory();
-  const logoutHandler = () => {
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        console.log("logged out");
-        history.go(0);
-        // Sign-out successful.
-      })
-      .catch((error) => {
-        // An error happened.
-      });
-  };
+  const [isShowingSearchBar, setIsShowingSearchBar] = useState(false);
+  // search from
+  const {
+    register,
+    handleSubmit,
+    watch,
+    getValues,
+    formState: { errors },
+  } = useForm<ISearchForm>({ mode: "onChange" });
 
-  const handleSearch = (searchTerm: string) => {
-    console.log("header", searchTerm);
+  const history = useHistory();
+
+  const handleSearch = (data: ISearchForm) => {
+    console.log("header", data);
     //      history.push({
     //        pathname: "/search",
     //        search: `?term=${searchTerm}`,
     //      });
-    history.push(`/search/${searchTerm}`);
+    history.push(`/search/${data.searchTerm}`);
   };
   return (
-    <>
+    <div className="container">
+      <form
+        className="justify-between bg-gray-700 text-white font-bold"
+        onSubmit={handleSubmit(handleSearch)}
+      >
+        {isShowingSearchBar ? (
+          <div className="flex flex-row justify-center">
+            <button type="button">
+              <FontAwesomeIcon
+                className="object-left mx-3"
+                size="1x"
+                icon={faArrowLeft}
+                onClick={() => setIsShowingSearchBar((prev) => !prev)}
+              />
+            </button>
+            <input
+              type="text"
+              {...register("searchTerm", {
+                required: false,
+              })}
+              className="w-9/12 border-none focus:outline-none pl-3 text-md text-black border-2 border-gray-800 my-3 rounded-md"
+            />
+            <button>
+              <FontAwesomeIcon
+                className=" mx-3 object-left border-1 border-red-800"
+                size="1x"
+                icon={faSearch}
+              />
+            </button>
+          </div>
+        ) : (
+          <div className="flex flex-row justify-between">
+            <Link to="/">
+              <img
+                className="hidden ml-5 w-32 md:block"
+                src={mainLogo}
+                alt=""
+              />
+              <img className="m-1 pt-1 w-20 md:hidden" src={mainLogo} alt="" />
+            </Link>
+            <div className="mt-4 mr-3">
+              <FontAwesomeIcon
+                className="object-left border-1 border-red-800"
+                size="1x"
+                icon={faSearch}
+                onClick={() => {
+                  setIsShowingSearchBar((prev) => !prev);
+                }}
+              />
+              <FontAwesomeIcon
+                onClick={() => setIsModalOpen((prev) => !prev)}
+                className="ml-3"
+                icon={faUser}
+              />
+            </div>
+          </div>
+        )}
+      </form>
       <div className="flex items-center w-full bg-gray-500 pt-5 pb-3">
-        <Link to="/">
-          <img className="hidden ml-5 w-32 md:block" src={mainLogo} alt="" />
-        </Link>
         <div className="flex flex-col mx-auto md:w-1/2 md:flex-row justify-center">
-          <input
-            type="text"
-            name="search-input"
-            value={searchTerm}
-            onChange={handleChange}
-            className="md:w-7/12 text-xl border-2 focus:outline-none border-gray-800 my-3 rounded-md"
-          />
-          <button
-            className="bg-red-700 py-1 px-2 ml-2 text-white font-semibold mb-3 rounded-md"
-            onClick={() => handleSearch(searchTerm)}
-          >
-            Search
-          </button>
           <button
             className="hidden btn bg-green-500 mt-0 ml-2 md:block"
             onClick={() => addVideoToWatchLaterList()}
@@ -72,15 +127,7 @@ export const Header: React.FC<IHeaderProps> = ({
             sorting
           </button>
         </div>
-        <div className="content-end">
-          <button
-            className="mr-3 btn bg-gray-800 text-white font-semibold"
-            onClick={() => logoutHandler()}
-          >
-            logout
-          </button>
-        </div>
       </div>
-    </>
+    </div>
   );
 };
